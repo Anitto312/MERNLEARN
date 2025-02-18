09c4a0de78622e4ad114f066ecbe5f3d17c7e624
Anitto312 <anitto312@gmail.com>
2025-02-18 08:25:14 +0530

    day7 commit

--- Day7/callback.js
+++ Day7/callback.js
@@ -1,10 +1,27 @@
-function Sjitclg(msg,abc){
-    console.log(msg);
-    abc()
+// function Sjitclg(msg,abc){
+//     console.log(msg);
+//     abc()
+// }
+
+// function Callbackeg(){
+//     console.log("welcome back");
+// }
+
+// Sjitclg("welcome",Callbackeg)
+
+
+function formsub(msg,cbf){
+    if(cbf()){
+        console.log(msg);
+    }
+    else{
+    console.log("unsuccess")
+    }
+}
+function formvalid(){
+    
+    console.log("Form success validation")
+    return true
 }
 
-function Callbackeg(){
-    console.log("welcome back");
-}
-
-Sjitclg("welcome",Callbackeg)
+formsub("submitted",formvalid)
