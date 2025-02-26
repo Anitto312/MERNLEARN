14a6b1a2a9f2f819af741fbb11f9a7c7e2834d58
Anitto312 <anitto312@gmail.com>
2025-02-26 13:50:26 +0530

    BAckend

--- sjitBackend/index.js
+++ sjitBackend/index.js
@@ -1,7 +1,8 @@
 const express = require("express");
 const mdb = require('mongoose');
-const dotenv = require('dotenv')
-const Signup = require('./models/signupSchema')
+const dotenv = require('dotenv');
+const bcrypt =require('bcrypt');
+const Signup = require('./models/signupSchema.js')
 
 const app = express();
 app.use(express.json())
@@ -17,19 +18,20 @@ mdb.connect(process.env.MONGODB_URL).then(() => {
 })
 
 app.get("/static",(req,res) => {
-    res.sendFile("C:\Users\anitt\OneDrive\Desktop\Mern\sjitFrontend\index.html")
+    res.sendFile("C://Users//anitt//OneDrive//Desktop//Mern//index.html")
 })
 
-app.post("/signup",(req,res) =>{
+app.post("/signup",async(req,res) =>{
     try{
         console.log(req.body);
         const {firstName,lastName,email,password,phoneNumber} = req.body
+        const hashPassword= await bcrypt.hash(password,10)
         const newSignup = new Signup({
             firstName:firstName,
-            lastName:lastName,
+            lastName:lastName, 
             email:email,
-            password:password,
-            phoneNumber:phoneNumber,
+            password:hashPassword,
+            phoneNumber:phoneNumber, 
 
         })
         newSignup.save()
@@ -40,6 +42,36 @@ app.post("/signup",(req,res) =>{
         console.log(error)
         res.status(201).json({message:"signup unsuccessfull",isSignup : false})
     }
+})
+app.get('/getsignupdet',async(req,res)=>{
+    const signup=await Signup.find()
+    console.log(signup)
+    res.send("fetched")
+})
+app.post("/login",async(req,res)=>{
+    try{
+        const {email,password}=req.body;
+        const existinguser=await Signup.findOne({email:email})
+        console.log(existinguser);
+        if(existinguser){
+            const isValidPassword=await bcrypt.compare(password,existinguser.password)
+            console.log(isValidPassword);
+            if(isValidPassword){
+                res.status(201).json({message:"success",isLoggedIn:true})
+            }
+            else{
+                res.status(201).json({message:"success",isLoggedIn:false})
+            }
+        }
+        else{
+            res.status(201).json({message:"success",isLoggedIn:false})
+        }
+    }
+    catch(err){
+        console.log("Login error");  
+        res.status(201).json({message:"Login error",isLoggedIn : false})
+    }
+
 })
 
 app.listen(PORT, () => console.log("Server Started successfully.",PORT));
